f2baf05a5af0212e3f61162ebf8c5da680e0a432
Pradeep Kumar Vikraman <pkumarvi@pkumarvi-mn4.linkedin.biz>
2018-07-25 01:32:23 -0700

    Blog functionality and polish

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/pkumarvi/pradeep_scripts/portfolio/myenv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
